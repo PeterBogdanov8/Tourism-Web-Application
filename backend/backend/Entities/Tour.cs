da2cf0bb206d347376f4d3b291adf5ed83a781c4
PeterBogdanov8 <peterbogdanov.pob@gmail.com>
2026-09-15 08:41:18 +0200

    Add the Tour entity

---
+++ backend/backend/Entities/Tour.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Entities
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
